bf00677860da7b04ad11990103961cbe6e438ab2
shaheen <01fe22bca103@kletech.ac.in>
2024-03-17 20:54:28 +0530

    code for devops

---
+++ DevOpsEngineer.java
@@ -0,0 +1,35 @@
+class DevOpsEngineer extends Employee {
+    private double bonus;
+
+    public DevOpsEngineer(int employeeId, String name, double salary, double bonus) {
+        super(employeeId, name, salary);
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public double calculateTotalSalary() {
+        return salary + bonus;
+    }
+
+    @Override
+    public void displayEmployeeDetails() {
+        super.displayEmployeeDetails();
+
+        System.out.println("Bonus: " + bonus);
+        System.out.println("Total Salary: " + calculateTotalSalary());
+    }
+}
+
+ class Main{
+    public static void main(String[] args) {
+        Employee employee = new Employee(1, "John Doe", 50000);
+        employee.displayEmployeeDetails();
+        System.out.println("\n");
+
+        DevOpsEngineer devOpsEngineer = new DevOpsEngineer(2, "Jane Doe", 60000, 5000);
+        devOpsEngineer.displayEmployeeDetails();
+    }
+}
